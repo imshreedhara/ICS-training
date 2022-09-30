bce5f0fb35b519b4d83da52933a6be2ba390c096
Shreedhara Ranganathappa <shreedharar@infinite.com>
2022-09-30 21:21:58 +0530

    till 2nd Assignment

--- Assignment_1/Assignment_1/Program.cs
+++ Assignment_1/Assignment_1/Program.cs
@@ -7,13 +7,15 @@ namespace Assignment_1
         {
             //EqualOrNot();
 
-            //PositiveOrNot();
+            NumIsPositiveOrNot();
 
             //Airthmatic_Operation();
 
-            //ForResult();
+            //AirthmaticOperation();
 
-            TempConversionExample();
+            //ForStudentResult();
+
+            //TempConversionExample();
 
             Console.ReadKey();
         }
@@ -41,7 +43,24 @@ namespace Assignment_1
                 Console.WriteLine("num1 & num2 are not quual");
             }
         }
-       
+
+
+        //Write a C# Sharp program to check whether a given number is positive or negative.  
+	    //Test Data : 14
+	    //Expected Output : 14 is a positive number
+
+        static void NumIsPositiveOrNot()
+        {
+            Console.WriteLine("Enter a number");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            if(num>=0)
+                Console.WriteLine("Given number is Positive");
+
+            else
+                Console.WriteLine("Given number is Negative");
+        }
+
 
         /* 3. Write a C# Sharp program that takes two numbers as input and perform an operation (+,-,*,/) on them and displays the result of that operation. 
 
@@ -51,16 +70,44 @@ namespace Assignment_1
                 Input second number: 12
                 Expected Output : 20 - 12 = 8      */
 
-        static void Airthmatic_Operation()
+        static void AirthmaticOperation()
         {
-            int x, y;
+            int option;
+            int n1 = 20;
+            int n2 = 12;
 
-            Console.WriteLine("Enter any 2 numbers");
+            Console.WriteLine("Option\t\tAction\n=========================");
+            Console.WriteLine("1\t\tAddition");
+            Console.WriteLine("2\t\tSubstract");
+            Console.WriteLine("3\t\tMultiplication");
+            Console.WriteLine("3\t\tDivision");
 
-            x = Convert.ToInt32(Console.ReadLine());
-            y = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Refer above menu and enter your option");
+            option = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("the value is:{0}", x - y);
+            switch (option)
+            {
+                case 1:
+                    Console.WriteLine(n1+n2);
+                    break;
+
+                case 2:
+                    Console.WriteLine(n1-n2);
+                    break;
+
+                case 3:
+                    Console.WriteLine(n1 * n2);
+                    break;
+
+                case 4:
+                    Console.WriteLine(n1 / n2);
+                    break;
+
+                default:
+                    Console.WriteLine("Invalid option");
+                    break;
+            }
+
         }
 
 
@@ -68,27 +115,42 @@ namespace Assignment_1
             calculates the average and prints result as passed or failed
             If avg< 50 then failed else passed   */
 
-        static void ForResult()
-         {
-             int CSharp, HTML, SQL;
+        static void ForStudentResult()
+        {
+            int Csharp;
+            int html;
+            int sql;
+            int total;
 
-             Console.WriteLine("Enter marks of all 3 subjects");
+            for (int i = 1; i <= 5; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    Console.WriteLine("Enter Csharp marks for Student " + i);
+                    Csharp = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Enter html marks for Student " + i);
+                    html = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Enter sql marks for Student" + i);
+                    sql = Convert.ToInt32(Console.ReadLine());
+                    total = Csharp + html + sql;
+                    total = total / 3;
 
-             for (int i = 1; i <= 5, i++)
-             {
-                 Console.WriteLine("Enter marks of CSharp : ");
-                 CSharp = Convert.ToInt32(Console.ReadLine());
+                    if (total < 50)
+                    {
+                        Console.WriteLine("total  mark for student " + i + " is : " + total);
+                        Console.WriteLine("student {0} is Failed", i);
+                    }
+                    else
+                    {
+                        Console.WriteLine("total  mark for student " + i + " is : " + total);
+                        Console.WriteLine("student {0} is passed", i);
+                    }
 
-                 Console.WriteLine("Enter marks of HTML : ");
-                 HTML = Convert.ToInt32(Console.ReadLine());
+                }
 
-                 Console.WriteLine("Enter marks of SQL : ");
-                 SQL = Convert.ToInt32(Console.ReadLine());
-             }
+            }
 
-         }
-
-
+        }
 
         //5.Write a program in C# to display temperature in Celsius. Accept the temperature in Fahrenheit.
 
@@ -101,7 +163,7 @@ namespace Assignment_1
             fahrenheit = Double.Parse(Console.ReadLine());
             Console.WriteLine("Fahrenheit: " + fahrenheit);
             celsius = (fahrenheit - 32) * 5 / 9;
-            Console.WriteLine("Celsius: " + celsius);
+            Console.WriteLine("Temperature in Celsius is : " + celsius);
         }
     }
 }
