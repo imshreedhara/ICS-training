487052308fbcbe6e3129078704232c58aff1e1e4
Shreedhara Ranganathappa <shreedharar@infinite.com>
2022-10-03 00:38:54 +0530

    Assignment 3.1

--- Assignment_3.2/Assignment_3.2/Properties/AssemblyInfo.cs
+++ Assignment_3.2/Assignment_3.2/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Assignment_3.1")]
+[assembly: AssemblyTitle("Assignment_3.2")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Assignment_3.1")]
+[assembly: AssemblyProduct("Assignment_3.2")]
 [assembly: AssemblyCopyright("Copyright ©  2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("1bdc7b7c-db81-4352-aabe-af590a9192ad")]
+[assembly: Guid("8142334b-0044-48ab-aaa2-f58e9333bd89")]
 
 // Version information for an assembly consists of the following four values:
 //
