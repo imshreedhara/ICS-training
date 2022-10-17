ef5f693c47a4625826f3b9fc11023ef3d1513466
Shreedhara Ranganathappa <shreedharar@infinite.com>
2022-10-17 10:17:04 +0530

    Code Test

---
+++ CodeBasedTest_1/CodeBasedTest_1_2/Program.cs
@@ -0,0 +1,19 @@
+﻿
+/* 2.Write a class Box that has Length and breadth as its members. Write a function that adds 2 box objects and 
+     stores in the 3rd. Create a Test class to execute the above  */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeBasedTest_1_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
