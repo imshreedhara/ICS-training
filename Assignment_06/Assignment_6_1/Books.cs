dbddb63e0037041f56aa78300c91190940a581e8
Shreedhara Ranganathappa <shreedharar@infinite.com>
2022-10-17 08:38:28 +0530

    Assignment-06

---
+++ Assignment_06/Assignment_6_1/Books.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*1. 
+   Create a class called Books with BookName and AuthorName members. Instantiate the class through constructor and also write a method Display() to 
+   display the details. Create an Indexer of Books Object to store 5 books in a class called BookShelf.
+ 
+   Using the indexer method assign values to the books and display the same.
+
+   (Hint : Use Composition. Books object inside BookShelf class)  */
+
+namespace Assignment_6_1
+{
+    class Books
+    {
+        public string BookName;
+        public string AuthorName;
+
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
